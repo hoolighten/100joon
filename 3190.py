5b2e64cdd5cbfff95c32d65e6812544ad5e07a27
hoolighten <94687936+hoolighten@users.noreply.github.com>
2023-03-03 23:10:27 +0900

    Update 3190.py

--- 3190.py
+++ 3190.py
@@ -6,11 +6,12 @@ def turn(_dir, arr):
     if _dir == "D":
         arr[0], arr[1], arr[2], arr[3] = b, c, d, a
     else:
-        arr[0], arr[1], arr[2], arr[3] = b, c, d, a
+        arr[0], arr[1], arr[2], arr[3] = d, a, b, c
     return arr
 
 
-times = 1
+info = []
+times = 0
 n = int(input())
 dir_array = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 snake_map = [[0 for i in range(n)] for j in range(n)]
@@ -24,18 +25,22 @@ queue = deque()
 queue.append((0, 0))
 l = int(input())
 for _ in range(l):
-    time, direction = input().split()
-    time = int(time)
-    for _ in range(time):
-        nx = nx + dir_array[0][0]
-        ny = ny + dir_array[0][1]
-        # if nx < 0 or nx >= n or ny < 0 or ny >= n or snake_map[nx][ny] == 1:
-        #     break
+    inf_turn = input().split()
+    info.append([int(inf_turn[0]), inf_turn[1]])
+
+while True:
+    nx = nx + dir_array[0][0]
+    ny = ny + dir_array[0][1]
+    if 0 <= nx < n and 0 <= ny < n and snake_map[nx][ny] != 1:
         if snake_map[nx][ny] == 0:
             x_tail, y_tail = queue.popleft()
             snake_map[x_tail][y_tail] = 0
-        times += 1
-        snake_map[nx][ny] = 1
-        queue.append((nx, ny))
-    dir_array = turn(direction, dir_array)
-print(times)
+    else:
+        break
+    snake_map[nx][ny] = 1
+    queue.append((nx, ny))
+    times += 1
+    for i in info:
+        if times == i[0]:
+            dir_array = turn(i[1], dir_array)
+print(times + 1)
