2edff4cac6c6f36386e62dcbb4a379c56892b298
hoolighten <94687936+hoolighten@users.noreply.github.com>
2023-07-12 16:27:03 +0900

    Add files via upload

--- 2275.py
+++ 2275.py
@@ -3,8 +3,11 @@ T = int(input())
 for i in range(T):
     floor = int(input())
     ho = int(input())
-    map = [i for i in range(1, ho+1)]
-    for k in range(floor):
-        for j in range(1, ho):
-            map[j] += map[j-1]
-    print(map[ho-1])
+    map = [i for i in range(0, ho+1)]
+    if floor == 0:
+        print(map[-1])
+    else:
+        for _ in range(floor):
+            for j in range(1, ho+1):
+                map[j] = map[j] + map[j-1]
+        print(map[-1])
