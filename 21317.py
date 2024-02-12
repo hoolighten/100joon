c1a7fc09024db9ec7ebeb37cf3f25425179ec28d
hoolighten <94687936+hoolighten@users.noreply.github.com>
2024-02-12 17:42:49 +0900

    Update 21317.py

--- 21317.py
+++ 21317.py
@@ -1,4 +1,5 @@
 import sys
+import copy
 
 input = sys.stdin.readline
 n = int(input())
@@ -15,16 +16,20 @@ if n >= 3:
         dp[i] = min(dp[i-1] + li_stone[i-1][0], dp[i-2] + li_stone[i-2][1])
     answer.append(dp[n-1])
     for i in range(3, n):
-        dp[i] = super_jump + dp[i-3]
+        dp1 = copy.deepcopy(dp)
+        dp1[i] = super_jump + dp1[i-3]
         if i+1 == n:
-            answer.append(dp[-1])
+            answer.append(dp1[-1])
             break
-        dp[i+1] = dp[i] + li_stone[i][0]
+        dp1[i+1] = dp1[i] + li_stone[i][0]
         for j in range(i+2, n):
-            dp[j] = min(dp[j-1] + li_stone[j-1][0], dp[j-2] + li_stone[j-2][1])
+            dp1[j] = min(dp1[j-1] + li_stone[j-1][0], dp1[j-2] + li_stone[j-2][1])
+    print(dp1)
     print(min(answer))
 elif n == 2:
     answer.append(li_stone[0][0])
     print(min(answer))
 else:
     print(0)
+
+
