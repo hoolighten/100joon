6ee8b5581ea463f34589bcdf5fed76813619624e
hoolighten <94687936+hoolighten@users.noreply.github.com>
2024-02-14 00:39:49 +0900

    Update 16926.py

--- 16926.py
+++ 16926.py
@@ -1,39 +1,47 @@
 import sys
 import copy
 
-def rotation(x, y):
-    for _ in range(n-1):
-        answer_graph[y+1][x] = graph[y][x]
-        y += 1
-        visited[y][x] = True
-    for _ in range(m-1):
-        answer_graph[y][x+1] = graph[y][x]
-        x += 1
-        visited[y][x] = True
-    for _ in range(n-1):
-        answer_graph[y-1][x] = graph[y][x]
-        y -= 1
-        visited[y][x] = True
-    for _ in range(m-1):
-        answer_graph[y][x-1] = graph[y][x]
-        x -= 1
-        visited[y][x] = True
-    
 
 input = sys.stdin.readline
+
+def rotation(x, y):
+    data = graph[y][x]
+    for _ in range(height-1):
+        next_data = graph[y+1][x]
+        graph[y+1][x] = data
+        data = next_data
+        y += 1
+    for _ in range(length-1):
+        next_data = graph[y][x+1]
+        graph[y][x+1] = data
+        data = next_data
+        x += 1
+    for _ in range(height-1):
+        next_data = graph[y-1][x]
+        graph[y-1][x] = data
+        data = next_data
+        y -= 1
+    for _ in range(length-1):
+        next_data = graph[y][x-1]
+        graph[y][x-1] = data
+        data = next_data
+        x -= 1
+    
+
 n, m, r = map(int, input().split())
 graph = list()
-visited = [[False]*m for i in range(n)]
-start_x, start_y = 0, 0
 for _ in range(n):
     graph.append(list(map(int, input().split())))
-answer_graph = copy.deepcopy(graph)
-while visited[start_y][start_x] == False:
-    for i in range(r):
+for i in range(r):
+    start_x, start_y = 0, 0
+    height, length = n, m
+    for _ in range(min(n, m) // 2):
         rotation(start_x, start_y)
-        graph = copy.deepcopy(answer_graph)
-    start_x += 1
-    start_y += 1
-    n -= 2
-    m -= 2
-print(answer_graph)
+        start_x += 1
+        start_y += 1
+        height -= 2
+        length -= 2
+for i in range(n):
+    for j in range(m):
+        print(graph[i][j], end=' ')
+    print()
