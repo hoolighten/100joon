9ba1869c3f1068d89292e7a38d1b2961f257614d
hoolighten <94687936+hoolighten@users.noreply.github.com>
2024-04-09 01:49:13 +0900

    Update 삼성코테_2차원테트리스.py

--- 삼성코테_2차원테트리스.py
+++ 삼성코테_2차원테트리스.py
@@ -8,13 +8,28 @@ def red_hei():
         max_val = max(red_height[x+r], max_val)
     return max_val
 
-
 def yel_hei():
     max_val = -1
     for x, y in blk[num]:
         max_val = max(yel_height[y+c], max_val)
     return max_val
 
+def Red_height():
+    for i in range(4):
+        for j in range(2, 6):
+            if red_arr[i][j] == 1:
+                red_height[i] = 7-j
+                break
+            else:
+                red_height[i] = 0
+def Yel_height():
+    for i in range(4):
+        for j in range(2, 6):
+            if yel_arr[j][i] == 1:
+                yel_height[i] = 7-j
+                break
+            else:
+                yel_height[i] = 0
 
 def red_tetris(d):
     _red_arr = copy.deepcopy(red_arr)
@@ -62,7 +77,8 @@ for i in range(k):
         else:
             yel_height[new_c] = y_h
         yel_arr[6 - y_h][new_c] = 1
-    #print('move graph',num, r, c)
+    #print('move graph', num, r, c)
+    #print('y-h',yel_height)
     #for i in range(6):
     #   print(*red_arr[i],' ', *yel_arr[i])
     #print('\n')
@@ -73,24 +89,27 @@ for i in range(k):
             if red_arr[j][i] == 1:
                 chk += 1
         if chk == 4:
-            #print('red_tetrio')
             over.append(i)
             
     for i in range(len(over)):
         score +=1
         red_tetris(over[i])
+        #print('red_tetrio')
+        Red_height()
         for k in range(4):
             if red_height[k] < 1:
                 red_height[k] = 0
             else:
                 red_height[k] -= 1
                 red_arr[k][0] = 0
-        for ov in range(2, 6):
-            if red_arr[new_r][ov] == 1:
-                red_height[new_r] = 6-ov
-            else:
-                red_height[new_r] = 0
+    
+    #     for ov in range(2, 6):
+    #         if red_arr[new_r][ov] == 1:
+    #             red_height[new_r] = 5-ov
+    #         else:
+    #             red_height[new_r] = 0
     #print("r-h",red_height)
+    #    print("r-h",yel_height)
     over.clear()
     
     # 옐로 테트리스일 경우 맨아래부터 확인
@@ -100,25 +119,27 @@ for i in range(k):
             if yel_arr[j][i] == 1:
                 chk += 1
         if chk == 4:
-            #print('yel_tetrio')
             over.append(j)
             
     for i in range(len(over)):
         score +=1
         yel_tetris(over[i])
+        #print('yel_tetrio',score)
+        Yel_height()
         for k in range(4):
             if yel_height[k] < 1:
                 yel_height[k] = 0
             else:
                 yel_height[k] -= 1
                 yel_arr[0][k] = 0
+    
         #print("y-h",yel_height)
         #print(over[i])
-        for ov in range(2,6):
-            if yel_arr[ov][new_c] == 1:
-                yel_height[new_c] = 6-ov
-            else:
-                yel_height[new_c] = 0
+        # for ov in range(2,6):
+        #     if yel_arr[ov][new_c] == 1:
+        #         yel_height[new_c] = 5 - ov
+        #     else:
+        #         yel_height[new_c] = 0
     #print("y-h",yel_height)
     over.clear()
     
@@ -148,12 +169,13 @@ for i in range(k):
                         yel_arr[0][k] = 0
 
     # 레드 테트리스일 경우 맨아래부터 확인
-
     #print('after_tetris_graph')
     #for i in range(6):
     #    print(*red_arr[i],' ', *yel_arr[i])
     #print('레드의 높이',red_height)
     #print('옐로의 높이',yel_height)
+    #print("y-h", yel_height)
+    #print("r-h", red_height)
 for i in range(6):
     for j in range(6):
         if red_arr[i][j] == 1:
